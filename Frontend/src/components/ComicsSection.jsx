3f7fd3546f706fba560bcf43b7c80310d8a7a791
ABDULLAH <141825353+RHETORICALWORDSMITH@users.noreply.github.com>
2024-09-22 03:09:33 +0500

    Update ComicsSection.jsx

--- Frontend/src/components/ComicsSection.jsx
+++ Frontend/src/components/ComicsSection.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ActionAnime from "./ActionAnime";
-import comicBackground from "../../public/homePics/comicBackground.png"
+import comicBackground from "../../public/homePics/comicBackground.png";
+
 const ComicsSection = () => {
   return (
-    <>
-      <div className=`relative overflow-hidden h-screen bg-[url(${comicBackground})] bg-cover bg-center bg-no-repeat bg-opacity-75`>
-        <div className="w-screen h-full bg-black opacity-80 absolute inset-0"></div>
-        <ActionAnime />
-      </div>
-     
-    </>
+    <div
+      className={`relative overflow-hidden h-screen bg-[url(${comicBackground})] bg-cover bg-center bg-no-repeat bg-opacity-75`}
+    >
+      <div className="w-screen h-full bg-black opacity-80 absolute inset-0"></div>
+      <ActionAnime />
+    </div>
   );
 };
 
