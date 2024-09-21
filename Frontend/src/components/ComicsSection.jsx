3cc1fe5047616a3f78216808bf6474ed11203e0a
ABDULLAH <141825353+RHETORICALWORDSMITH@users.noreply.github.com>
2024-09-22 03:14:21 +0500

    Update ComicsSection.jsx

--- Frontend/src/components/ComicsSection.jsx
+++ Frontend/src/components/ComicsSection.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ActionAnime from "./ActionAnime";
-import comicBackground from "../../public/homePics/comicBackground.png";
 
 const ComicsSection = () => {
   return (
     <div
-      className={`relative overflow-hidden h-screen bg-[url(${comicBackground})] bg-cover bg-center bg-no-repeat bg-opacity-75`}
+      className={`relative overflow-hidden h-screen bg-[url('/homePics/comicBackground.png')] bg-cover bg-center bg-no-repeat`}
     >
       <div className="w-screen h-full bg-black opacity-80 absolute inset-0"></div>
       <ActionAnime />
