149394f8afbea2c49edea2242091ea642625e08d
ABDULLAH <141825353+RHETORICALWORDSMITH@users.noreply.github.com>
2024-09-22 03:41:36 +0500

    Update multer.js

--- Backend/config/multer.js
+++ Backend/config/multer.js
@@ -1,7 +1,7 @@
 import multer from "multer";
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      return cb(null, "../Frontend/public");
+      return cb(null, "/public");
     },
     filename: function (req, file, cb) {
       return cb(null, `${Date.now()}-${file.originalname}`);
