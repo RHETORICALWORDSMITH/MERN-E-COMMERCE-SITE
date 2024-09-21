65a5442be4f3f77d6afd00da46a803e687ad2c00
ABDULLAH <141825353+RHETORICALWORDSMITH@users.noreply.github.com>
2024-09-21 18:37:49 +0500

    Update CartCards.jsx

--- Frontend/src/components/CartCards.jsx
+++ Frontend/src/components/CartCards.jsx
@@ -76,7 +76,7 @@ const CartCards = () => {
     // sending post rquest to send data to the backend
     try {
       const res = await axios.post(
-        "http://localhost:3000/history/",
+        "https://mern-e-commerce-site-six.vercel.app/history/",
         dataToSend
       );
       console.log(res.data);
