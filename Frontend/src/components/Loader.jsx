69505688667dffd12b16d27e6e72d2330cb486d2
ABDULLAH <141825353+RHETORICALWORDSMITH@users.noreply.github.com>
2024-09-22 03:46:18 +0500

    Update Loader.jsx

--- Frontend/src/components/Loader.jsx
+++ Frontend/src/components/Loader.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 const Loader = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-purple-500">
-      <div className="loader border-t-transparent border-solid rounded-full animate-spin border-4 border-red-500 w-16 h-16"></div>
+    <div className="flex justify-center items-center h-screen bg-black">
+      <div className="loader border-t-transparent border-solid rounded-full animate-spin animate-pulse border-4 border-red-500 w-32 h-32"></div>
     </div>
   );
 };
